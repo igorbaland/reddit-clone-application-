cb67ef4f422558b6aa7438f131a1fe5debe35a29
Igor <igor.balandin@gmx.de>
2023-07-14 05:06:38 +0200

    main class

---
+++ backend-reddit-clone/src/main/java/com/example/backendredditclone/BackendRedditCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendredditclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendRedditCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendRedditCloneApplication.class, args);
+	}
+
+}
